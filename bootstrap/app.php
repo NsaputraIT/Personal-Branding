f55864ae4436231e4cdeab1294322a1109be12b0
langsvio <jihan.violita27@gmail.com>
2026-07-16 13:10:03 +0700

    fix(SSL):fix force HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Percayai proxy Cloudflare agar deteksi HTTPS dari reverse proxy akurat
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
